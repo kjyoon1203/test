e7eef483a7d2632754880edbeadc977f2330fec5
kjyoon <kjyoon1203@gmail.com>
2019-07-31 16:45:38 +0900

    jspTest 최초 커밋

---
+++ src/main/java/kr/or/ddit/Test.java
@@ -0,0 +1,30 @@
+package kr.or.ddit;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Test extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html;charset=utf-8");
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = new Date();
+		String today = sdf.format(date);
+		
+		PrintWriter pw = resp.getWriter();
+		pw.write("<html>");
+		pw.write("	<body>");
+		pw.write("<h1>hello, today: "+today+"</h1>");
+		pw.write("	</body>");
+		pw.write("</html>");
+	}
+	
+}
